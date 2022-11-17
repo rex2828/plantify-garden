713098ac57ceab05649359ce68be4245185bd376
ash97531 <ashwani.97531@gmail.com>
2022-11-17 22:47:00 +0530

    first commit

---
+++ src/firebase-config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC-OrZuyPrqD6mhtuo-eYHLMeHAkXer_L8",
+    authDomain: "plant-app-d4907.firebaseapp.com",
+    projectId: "plant-app-d4907",
+    storageBucket: "plant-app-d4907.appspot.com",
+    messagingSenderId: "183907028562",
+    appId: "1:183907028562:web:6c83b592a26a9e89b3d98b",
+    measurementId: "G-3YG4X96V98"
+  };
+
+export const app = initializeApp(firebaseConfig);
